18d86c0b6d408f002c485b7314af410f4f4a31d1
Nino Schelcher <onin1998@gmail.com>
2020-04-14 17:23:18 +0200

    time and progressbar

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const nextButton = document.querySelector('.next');
 let currentTime = document.querySelector(".current");
 let restTime = document.querySelector('.left');
 const audio = new Audio("netsky.mp3");
+let progressBar = document.querySelector(".progressBar");
+
 
 playButton.addEventListener('click', function () {
     audio.play();
@@ -20,19 +22,38 @@ pauseButton.addEventListener('click', function () {
     event.preventDefault();
 })
 
-audio.addEventListener("timeupdate", function () {
-    let seconds = parseInt(audio.currentTime % 60);
-    let minutes = parseInt((audio.currentTime / 60) % 60);
-    currentTime.innerHTML = minutes + ':' + seconds;
-}, false);
-
 previousButton.addEventListener('click', function () {
     audio.currentTime = 0;
     audio.play();
 })
 
 nextButton.addEventListener('click', function () {
-    audio.currentTime= audio.currentTime + 5;
+    audio.currentTime = audio.currentTime + 5;
 })
 
-console.log(audio.volume)
+audio.addEventListener("timeupdate", function () {
+    currentTime.innerHTML = currentAudio(audio.currentTime);
+    progressBar.style.width =  audio.currentTime * 100 / audio.duration + "%";
+})
+
+function currentAudio(seconds) {
+    minutes = Math.floor(seconds / 60);
+    minutes = minutes >= 10 ? minutes : "0" + minutes;
+    seconds = Math.floor(seconds % 60);
+    seconds = seconds >= 10 ? seconds : "0" + seconds;
+    return minutes + ":" + seconds;
+}
+
+audio.addEventListener("timeupdate", function () {
+    var timeleft = document.querySelector('.left'),
+        songDuration = parseInt( audio.duration ),
+        currentAudioTime = parseInt( audio.currentTime ),
+        audioTimeLeft = songDuration - currentAudioTime,
+        seconds, minutes;
+    
+    seconds = audioTimeLeft % 60;
+    minutes = Math.floor( audioTimeLeft / 60 ) % 60;
+    seconds = seconds < 10 ? "0"+seconds : seconds;
+    minutes = minutes < 10 ? "0"+minutes : minutes;
+    timeleft.innerHTML = minutes+":"+seconds;
+}, false);
